af609e146fb3bba2fa0b40b9262364bf11fbc786
Lucas Martins <lucaslealm@gmail.com>
2019-02-28 17:00:15 -0300

    green rake

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, :cocktail_id, :ingredient_id, presence: true
-  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+  validates :description, presence: true
+  validates :ingredient, uniqueness: { scope: :cocktail }
 end
